775009e900cc2b30ee46f104fed005a22c4a3c10
Rushberry <info.rushberry@gmail.com>
2025-05-22 23:29:16 +0600

    Initialized with Github Repo

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <div className="h-screen w-full bg-[url('/src/assets/error.gif')] bg-cover bg-no-repeat relative bg-bottom">
+            <h1 className="text-white text-center pt-10 text-5xl font-space font-extrabold">Page Not Found</h1>
+            <Link to={'/'} className="absolute bottom-5 right-5 text-black font-medium rounded-lg font-space bg-white px-2 py-1">Back to Home</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className="text-red-800 text-center">Hello</p>
+            <img src="/src/assets/error.gif" alt="" />
+        </div>
+    );
+};
+
+export default Home;
